e1d44d87a72024904c7cda88ec374f6d657c2b90
Maxim <msirotkin95@gmail.com>
2024-07-02 20:52:48 +0300

    Step4

--- src/main/java/hexlet/code/config/SecurityConfig.java
+++ src/main/java/hexlet/code/config/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/login").permitAll()
                         .requestMatchers("/api/pages/*").permitAll()
                         .requestMatchers("/api/pages").permitAll()
+                        .requestMatchers("/api/welcome").permitAll()
                         .requestMatchers("/").permitAll()
                         .requestMatchers("/index.html").permitAll()
                         .requestMatchers("/assets/**").permitAll()
