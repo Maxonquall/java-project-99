e2792d36049626f87e14ecd355908d4aa7dadfa9
Maxim <msirotkin95@gmail.com>
2024-07-21 18:17:35 +0300

    Step9

--- src/test/java/hexlet/code/controller/api/LabelControllerTest.java
+++ src/test/java/hexlet/code/controller/api/LabelControllerTest.java
@@ -18,7 +18,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import hexlet.code.controller.api.util.ModelGenerator;
 import hexlet.code.repository.TaskStatusRepository;
 import hexlet.code.repository.UserRepository;
-//import jakarta.transaction.Transactional;
+import jakarta.transaction.Transactional;
 import org.instancio.Instancio;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -28,7 +28,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor;
-import org.springframework.test.annotation.DirtiesContext;
+//import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -43,7 +43,145 @@ import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@Transactional
+public class LabelControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private Faker faker;
+
+    @Autowired
+    private LabelRepository labelRepository;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ObjectMapper om;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Autowired
+    private TaskStatusRepository taskStatusRepository;
+
+    @Autowired
+    private ModelGenerator modelGenerator;
+
+    private JwtRequestPostProcessor token;
+
+    private Label testLabel;
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac)
+                .defaultResponseCharacterEncoding(StandardCharsets.UTF_8)
+                .apply(springSecurity())
+                .build();
+
+        String uniqueEmail = faker.internet().emailAddress();
+        token = jwt().jwt(builder -> builder.subject(uniqueEmail));
+
+        testLabel = Instancio.of(modelGenerator.getLabelModel())
+                .create();
+        labelRepository.save(testLabel);
+    }
+
+    @AfterEach
+    public void clear() {
+        taskRepository.deleteAll();
+        taskStatusRepository.deleteAll();
+        userRepository.deleteAll();
+        labelRepository.deleteAll();
+    }
+
+    @Test
+    public void testIndex() throws Exception {
+        var request = get("/api/labels")
+                .with(token);
+
+        var result = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var body = result.getResponse().getContentAsString();
+        var labels = om.readValue(body, new TypeReference<List<Label>>() {});
+        var expected = labelRepository.findAll();
+
+        assertThat(labels.containsAll(expected)).isTrue();
+    }
+
+    @Test
+    public void testCreate() throws Exception {
+        var data = Instancio.of(modelGenerator.getLabelModel()).create();
+
+        var labelsCount = labelRepository.count();
+
+        var request = post("/api/labels")
+                .with(token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(om.writeValueAsString(data));
+
+        mockMvc.perform(request)
+                .andExpect(status().isCreated());
+
+        assertThat(labelRepository.count()).isEqualTo(labelsCount + 1);
+
+        var addedLabel = labelRepository.findByName(data.getName()).get();
+
+        assertNotNull(addedLabel);
+        assertThat(labelRepository.findByName(testLabel.getName())).isPresent();
+
+        assertThat(addedLabel.getName()).isEqualTo(data.getName());
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        var oldName = testLabel.getName();
+        var newName = faker.lorem().word();
+
+        var data = new HashMap<>();
+        data.put("name", newName);
+
+        var labelsCount = labelRepository.count();
+
+        var request = put("/api/labels/" + testLabel.getId())
+                .with(token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(om.writeValueAsString(data));
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk());
+
+        assertThat(labelRepository.count()).isEqualTo(labelsCount);
+
+        var label = labelRepository.findById(testLabel.getId()).get();
+
+        assertThat(label.getName()).isEqualTo(newName);
+        assertThat(labelRepository.findByName(oldName)).isEmpty();
+    }
+
+    @Test
+    public void testDestroy() throws Exception {
+        var labelsCount = labelRepository.count();
+
+        token = jwt().jwt(builder -> builder.subject(testLabel.getName()));
+
+        mockMvc.perform(delete("/api/labels/" + testLabel.getId()).with(token))
+                .andExpect(status().isNoContent());
+
+        assertThat(labelRepository.count()).isEqualTo(labelsCount - 1);
+        assertThat(labelRepository.findById(testLabel.getId())).isEmpty();
+    }
+}
+/*@SpringBootTest
+@AutoConfigureMockMvc
+//@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 //@Transactional
 public class LabelControllerTest {
 
@@ -180,5 +318,5 @@ public class LabelControllerTest {
         assertThat(labelRepository.findById(testLabel.getId())).isEmpty();
     }
 
-}
+}*/
 
